39a449054ca1b6dd5809ae4ff36a0706be6c443f
Marshall Bowers <elliott.codes@gmail.com>
2024-01-14 10:31:40 -0500

    Add `loading` attribute method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- `HtmlElement`: Added new attribute methods:
+  - `loading`
+
 ## [0.3.0] - 2024-01-13
 
 ### Added
--- src/lib.rs
+++ src/lib.rs
@@ -165,8 +165,8 @@ html_elements!(
     a, abbr, address, area, article, aside, audio, b, base, bdi, bdo, blockquote, body, br, button,
     canvas, caption, cite, code, col, colgroup, data, datalist, dd, del, dfn, div, dl, dt, em,
     embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, head, header,
-    hgroup, hr, html, i, iframe, img, input, ins, kbd, label, legend, li, link, main, map, mark,
-    math, menu, meta, meter, nav, noscript, object, ol, optgroup, option, output, p, picture,
+    hgroup, hr, html, i, iframe, img, input, ins, kbd, label, legend, li, link, loading, main, map,
+    mark, math, menu, meta, meter, nav, noscript, object, ol, optgroup, option, output, p, picture,
     portal, pre, progress, q, rp, rt, ruby, s, samp, script, search, section, select, small,
     source, span, strong, style, sub, sup, svg, table, td, textarea, tfoot, th, thead, time, title,
     tr, track, u, ul, var, video, wbr, details, dialog, summary, slot, template
