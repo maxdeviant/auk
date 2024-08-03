f0226b3d760b3c4eebab7d858cbfee933ebcb841
Marshall Bowers <elliott.codes@gmail.com>
2024-08-02 21:16:54 -0400

    Make `is_void` method `pub`

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ impl HtmlElement {
     }
 
     /// Returns whether this element is a [void element](https://developer.mozilla.org/en-US/docs/Glossary/Void_element).
-    fn is_void(&self) -> bool {
+    pub fn is_void(&self) -> bool {
         match self.tag_name.as_str() {
             "area" | "base" | "br" | "col" | "embed" | "hr" | "img" | "input" | "link" | "meta"
             | "param" | "source" | "track" | "wbr" => true,
