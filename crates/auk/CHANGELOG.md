62ce7dc7abb4f6a5ad14909759a15e4111b2401e
Marshall Bowers <git@maxdeviant.com>
2025-12-21 21:38:19 -0500

    auk: Add `html` and `text` conversion methods to `Element`

--- crates/auk/CHANGELOG.md
+++ crates/auk/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Added `Render` trait.
+- Added `html` and `text` conversion methods to `Element`.
 
 ## [0.6.0] - 2024-12-18
 
--- crates/auk/src/element.rs
+++ crates/auk/src/element.rs
@@ -9,3 +9,21 @@ pub enum Element {
     /// An HTML element.
     Html(HtmlElement),
 }
+
+impl Element {
+    /// Converts this [`Element`] into a [`TextElement`].
+    pub fn text(self) -> Option<TextElement> {
+        match self {
+            Self::Text(text) => Some(text),
+            Self::Html(_) => None,
+        }
+    }
+
+    /// Converts this [`Element`] into an [`HtmlElement`].
+    pub fn html(self) -> Option<HtmlElement> {
+        match self {
+            Self::Html(html) => Some(html),
+            Self::Text(_) => None,
+        }
+    }
+}
